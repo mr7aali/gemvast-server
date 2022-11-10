720af39181249c721abd73a24c32da66a93b2062
mr7aali <mr7aali@gmail.com>
2022-11-10 06:05:25 +0600

    loded service data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -16,53 +16,60 @@ app.use(express.json());
 const uri = "mongodb+srv://gemvast:GAvUcicPpNfcq8lQ@cluster0.lopokh6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        const userCollection  = client.db("gemVast").collection('services');
-        const reviewCollection  = client.db("gemVast").collection('review');
+async function run() {
+    try {
+        const userCollection = client.db("gemVast").collection('services');
+        const reviewCollection = client.db("gemVast").collection('review');
 
-        app.get('/service',async(req,res)=>{
-            const query ={};
-            const cursor  = userCollection.find(query);
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
 
         })
-        app.get('/allservice',async(req,res)=>{
-            const query ={};
-            const cursor  = userCollection.find(query);
+        app.get('/allservice', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
-       // services/
-        app.get('/services/:id',async(req,res)=>{
+        // services/
+        app.get('/services/:id', async (req, res) => {
 
-            const ID= req.params.id;
-          //  console.log(ID)
-             const query ={_id:ObjectId(ID)}
-            // console.log(query)
-             const service  = await userCollection.findOne(query)          
-             res.send(service)
+            const ID = req.params.id;
+            const query = { _id: ObjectId(ID) }
+            const service = await userCollection.findOne(query)
+            res.send(service)
         })
 
         ///post
-        app.post('/services',async(req,res)=>{
+        app.post('/services', async (req, res) => {
             const review = req.body;
-            //console.log(review)
             const result = await reviewCollection.insertOne(review);
-           res.send(result)
+            res.send(result)
+        }) 
+
+
+        app.get('/myreviews/:email', async (req, res) => {
+            const Email = req.params.email;
+            const query = {email:Email};
+            const cursor = await reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
         })
 
+
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch(er=>console.log(er))
-app.get('/',(req,res)=>{
+run().catch(er => console.log(er))
+app.get('/', (req, res) => {
     res.send("node server runing ");
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Node server runnig port ${port}`);
 })
